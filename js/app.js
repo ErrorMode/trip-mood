e06bbac8d9d3e3d864bf05b0ff10c952d59f3eea
Alexandros Ntalianis <alexandros_ntalianis@hotmail.com>
2019-04-15 16:23:15 +0300

    js so far

---
+++ js/app.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(init);
+
+
+//Same as: jQuery(document).ready(init);
+
+//Our application goes here:
+function init(){
+    let options = {
+        url : "data.json",
+        success: jsonHandler
+    }
+
+    function jsonHandler(data){
+        
+        console.log(data);
+    }
+
+    $.ajax(options);
+}
+
+//Alternative,no global vars at all:
+//jQuery (function init()){ ... });
