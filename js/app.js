5937f13f36e263c884a53126a9b655c76338d5bf
Alexandros Ntalianis <alexandros_ntalianis@hotmail.com>
2019-04-16 13:52:28 +0300

    jsonHandler update

--- js/app.js
+++ js/app.js
@@ -4,15 +4,35 @@ jQuery(document).ready(init);
 //Same as: jQuery(document).ready(init);
 
 //Our application goes here:
-function init(){
+function init() {
     let options = {
-        url : "data.json",
+        url: "data.json",
         success: jsonHandler
     }
 
-    function jsonHandler(data){
-        
-        console.log(data);
+    function removeDups(names) {
+        let unique = {};
+        names.forEach(function (i) {
+            if (!unique[i]) { unique[i] = true; }
+        });
+        return Object.keys(unique);
+    }
+
+    function jsonHandler(data) {
+
+        let entries = data[1].entries;
+        let cities = entries.map(getCity);
+
+        //console.log(entries); //Array [ {HOTEL} x 4 ]
+
+        function getCity(hotel) {
+            return hotel.city;
+            //console.log(hotel.city);
+        }
+
+        let uniqueCities = removeDups(cities);
+        console.log(uniqueCities);
+
     }
 
     $.ajax(options);
