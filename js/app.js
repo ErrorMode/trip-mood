cacf391f6f42cf8831dcc216a1b741f0ff0e3e85
Alexandros Ntalianis <alexandros_ntalianis@hotmail.com>
2019-04-16 14:40:15 +0300

    update datalist insert

--- js/app.js
+++ js/app.js
@@ -18,21 +18,44 @@ function init() {
         return Object.keys(unique);
     }
 
+    function showOptions(listOfCities){
+        //1)get datalist
+        let datalist = document.querySelector("#cities");
+        console.log(datalist); //Testing
+
+        //2)loop over unique cities array
+        listOfCities.map(addOption);
+
+        
+        function addOption(city){
+            let option = `<option value="${city}"></option>` 
+            datalist.innerHTML += option;
+           
+        }
+
+        //  2.1)Inside Loop:
+        //     --Create option element
+        //     --Add city value
+        //     --Add to datalist(innerHTML)
+    }
+
     function jsonHandler(data) {
-
-        let entries = data[1].entries;
-        let cities = entries.map(getCity);
-
-        //console.log(entries); //Array [ {HOTEL} x 4 ]
 
         function getCity(hotel) {
             return hotel.city;
             //console.log(hotel.city);
         }
 
+        let entries = data[1].entries;
+        let cities = entries.map(getCity);
         let uniqueCities = removeDups(cities);
-        console.log(uniqueCities);
 
+        //console.log(entries); //Array [ {HOTEL} x 4 ]
+
+       // console.log(uniqueCities);
+        showOptions(uniqueCities);
+
+    
     }
 
     $.ajax(options);
